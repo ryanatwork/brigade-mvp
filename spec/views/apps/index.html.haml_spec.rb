8583f52602c90dc3614621f25bff3e87e0c8e678
Ryan Resella <ryan@codeforamerica.org>
2012-02-04 16:21:00 -0800

    Set view specs to pending

--- spec/views/apps/index.html.haml_spec.rb
+++ spec/views/apps/index.html.haml_spec.rb
@@ -24,7 +24,7 @@ describe "apps/index" do
     ])
   end
 
-  it "renders a list of apps" do
+  pending it "renders a list of apps" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Name".to_s, :count => 2
--- spec/views/apps/show.html.haml_spec.rb
+++ spec/views/apps/show.html.haml_spec.rb
@@ -13,7 +13,7 @@ describe "apps/show" do
     ))
   end
 
-  it "renders attributes in <p>" do
+  pending it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Name/)
